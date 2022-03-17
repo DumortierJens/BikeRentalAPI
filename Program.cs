14782ebba11efe778a76936ec736332acb71ca89
Jens Dumortier <72012366+DumortierJens@users.noreply.github.com>
2022-03-17 20:14:16 +0100

    Add MongoContext to builder

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
+
+var mongoSettings = builder.Configuration.GetSection("MongoConnection");
+builder.Services.Configure<DatabaseSettings>(mongoSettings);
+builder.Services.AddTransient<IMongoContext, MongoContext>();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "API is working!");
