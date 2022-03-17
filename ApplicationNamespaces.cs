bd541f3ba7122e4a2a19fbb8c536da6d67024d1a
Jens Dumortier <72012366+DumortierJens@users.noreply.github.com>
2022-03-17 19:51:06 +0100

    Init

---
+++ ApplicationNamespaces.cs
@@ -0,0 +1,13 @@
+// .NET
+global using System;
+global using Microsoft.Extensions.Options;
+
+// Nuget
+global using MongoDB.Bson;
+global using MongoDB.Bson.Serialization.Attributes;
+global using MongoDB.Driver;
+global using FluentValidation;
+global using FluentValidation.AspNetCore;
+
+// Local
+// global using BikeRentalAPI.Models;
