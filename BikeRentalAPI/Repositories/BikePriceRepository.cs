7b7088c189f0898962102dc2758c6dd3991b6412
Jens Dumortier <72012366+DumortierJens@users.noreply.github.com>
2022-03-23 18:14:51 +0100

    Bike Price Repository

---
+++ BikeRentalAPI/Repositories/BikePriceRepository.cs
@@ -0,0 +1,29 @@
+namespace BikeRentalAPI.Repositories;
+
+public class BikePriceRepository
+{
+    private readonly IMongoContext _context;
+
+    public BikePriceRepository(IMongoContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<BikePrice>> GetBikePricesByLocation(Location location) => await _context.BikePriceCollection.Find(_ => (_.Location != null && _.Location.Id == location.Id)).ToListAsync();
+
+    public async Task<BikePrice> GetBikePrice(Location location, Bike bike) => await _context.BikePriceCollection.Find(_ => (_.Location != null && _.Location.Id == location.Id && _.Bike != null && _.Bike.Id == bike.Id)).FirstOrDefaultAsync();
+
+    public async Task<BikePrice> AddBikePrice(BikePrice bikePrice)
+    {
+        await _context.BikePriceCollection.InsertOneAsync(bikePrice);
+        return bikePrice;
+    }
+
+    public async Task<BikePrice> UpdateBikePrice(BikePrice bikePrice)
+    {
+        var filter = Builders<Bike>.Filter.Eq("id", bikePrice.Id);
+        var update = Builders<Bike>.Update.Set("prices", bikePrice.Prices);
+        var result = await _context.BikeCollection.UpdateOneAsync(filter, update);
+        return bikePrice;
+    }
+}
