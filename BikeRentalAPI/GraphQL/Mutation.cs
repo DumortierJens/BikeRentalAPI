841922665e1ab8c103aa556cb577137e929a4c69
Jens Dumortier <72012366+DumortierJens@users.noreply.github.com>
2022-04-21 12:25:51 +0200

    Start Rental Mutation

--- BikeRentalAPI/GraphQL/Mutation.cs
+++ BikeRentalAPI/GraphQL/Mutation.cs
@@ -2,6 +2,19 @@ namespace BikeRentalAPI.GraphQL.Mutations;
 
 public class Mutation
 {
+    public async Task<StartRentalPayload> StartRental([Service] IRentalService rentalService, StartRentalInput input)
+    {
+        var newRental = new Rental()
+        {
+            Name = input.name,
+            Tel = input.tel,
+            Location = new RentalLocation() { Id = input.locationId },
+            Bike = new Bike() { Id = input.bikeId }
+        };
+        var rental = await rentalService.StartRental(newRental);
+        return new StartRentalPayload(rental);
+    }
+
     public async Task<UpdateRentalDetailsPayload> UpdateRentalDetails([Service] IRentalService rentalService, UpdateRentalDetailsInput input)
     {
         var updatedRentalDetails = new Rental()
